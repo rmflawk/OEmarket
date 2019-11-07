cd061b2d5252b2d146dc8427ee4ea30f7f69fc5f
rmflawk <ant921111@naver.com>
2019-11-07 17:31:07 +0900

    가지 추가

--- app/src/main/java/com/youngstudio/oemarket/Item.java
+++ app/src/main/java/com/youngstudio/oemarket/Item.java
@@ -14,4 +14,38 @@ public class Item {
         this.img = img;
     }
 
+
+    public String getMsg1() {
+        return msg1;
+    }
+
+    public void setMsg1(String msg1) {
+        this.msg1 = msg1;
+    }
+
+    public String getMsg2() {
+        return msg2;
+    }
+
+    public void setMsg2(String msg2) {
+        this.msg2 = msg2;
+    }
+
+    public String getMsg3() {
+        return msg3;
+    }
+
+    public void setMsg3(String msg3) {
+        this.msg3 = msg3;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+
 }
